fee8ea5da7ca78e01fb8f6859867caa1f3496ac8
Romane Valenti <romane.valenti@gmail.com>
2023-08-08 17:52:44 +0200

    Finalisation CRUD pour transaction
    Les methodes CRUD sont terminées et fonctionnels pour la table transaction

--- controllers/ctrl.js
+++ controllers/ctrl.js
@@ -2,11 +2,44 @@ import transactionValidation from "../validation/transactionValidation.js";
 import Transaction from "../models/transaction.js";
 
 
-const getAllTransactions = () => {};
-const getOneTransaction = () => {};
+//------------------METHODE GET ALL TRANSACTIONS-------------------------------------------------------------------------------------------------
 
-//--------------------------------------------------------------------------------------------------------------------------------
-//Methode POST
+
+// Cette fonction récupère toutes les transactions de la base de données
+const getAllTransactions = (req, res) => {
+
+    // Utilisation de la méthode findAll() de Sequelize pour récupérer toutes les transactions
+    Transaction.findAll({
+        // Exclure les colonnes 'createdAt' et 'updatedAt' des résultats
+        attributes: {exclude: ['createdAt', 'updatedAt']}
+    })
+    .then(transactions => res.status(200).json(transactions))
+    .catch(error => res.status(500).json(error));
+};
+
+
+
+
+//------------------METHODE GET ONE TRANSACTIONS-------------------------------------------------------------------------------------------------
+
+//Récupère une transaction par son id
+const getOneTransaction = (req, res) => {
+    const {id} = req.params;
+
+    Transaction.findByPk(id)
+   .then(transaction => {
+    if(!transaction) return res.status(404).json({message: 'Transaction not found'});
+    return res.status(200).json(transaction);
+   })
+   .catch(error => res.status(500).json(error));
+};
+
+
+
+
+//---------------------- METHODE POST----------------------------------------------------------------------------------------------------------
+
+//Créer transaction 
 const createOneTransaction = (req, res) => {
     //La fonction récupère le corps de la requête
     const {body} = req;
@@ -23,7 +56,45 @@ const createOneTransaction = (req, res) => {
 
 };
 
-const updateOneTransaction = () => {};
-const deleteOneTransaction = () => {};
+
+//------------------METHODE PUT ----------------------------------------------------------------------------------------------------------
+
+//Modifie une transaction par son id
+const updateOneTransaction = (req, res) => {
+    const {body} = req;
+    const {id} = req.params;
+
+    Transaction.findByPk(id)
+    .then(transaction => {
+
+         // Vérifier si la transaction existe
+        if(!transaction) return res.status(404).json({message: 'Transaction not found'})
+
+        // Mettre à jour les propriétés de la transaction avec les valeurs du corps de la requête
+        transaction.transaction_amount = body.transaction_amount;
+        transaction.date = body.date;
+        transaction.transaction_type = body.transaction_type;
+        transaction.description = body.description;
+
+        // Enregistrer les modifications dans la base de données
+        transaction.save()
+        .then(() => res.status(200).json({message: 'Transaction updated successfully'}))
+        .catch(error => res.status(500).json(error));
+    })
+    .catch(error => res.status(500).json(error));
+};
+
+// ------------------METHODE DELETE TRANSACTION----------------------------------------------------------------------------------------------------------
+
+const deleteOneTransaction = (req, res) => {
+    const {id} = req.params;
+    
+        Transaction.destroy({where: {id: id}})
+        .then(ressource => {
+            if(ressource === 0) return res.status(404).json({message: 'Transaction not found'})
+            res.status(200).json({message: 'Transaction deleted successfully'})
+        })
+        .catch((error) => res.status(500).json(error));
+};
 
 export {getAllTransactions, getOneTransaction, createOneTransaction, updateOneTransaction, deleteOneTransaction};
--- validation/transactionValidation.js
+++ validation/transactionValidation.js
@@ -1,11 +1,14 @@
 import Joi from 'joi';
 
+
+// Cette fonction effectue la validation des données de transaction
 const transactionValidation = (body) => {
+    //Création du schema de la transaction
     const TransactionSchema = Joi.object({
         user_id: Joi.number().required(),
         transaction_amount: Joi.number().required().min(0),
         description: Joi.string(),
-        date: Joi.date(),
+        date: Joi.date().required(),
         payment_method: Joi.string().required().allow('carte de crédit').allow('monnaie').allow('virement').allow('chèque').allow('paypal'),
         category_id: Joi.string().required(),
         type_transaction: Joi.string().required().allow('dépense').allow('recette'),
