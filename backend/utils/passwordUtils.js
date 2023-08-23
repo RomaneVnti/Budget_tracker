777aee9c60dc1de477fb31b8cc105603290d5461
Romane Valenti <romane.valenti@gmail.com>
2023-08-23 18:58:09 +0200

    Update utils.js

--- backend/utils/passwordUtils.js
+++ backend/utils/passwordUtils.js
@@ -1,13 +1,15 @@
 import bcrypt from 'bcrypt';
 
-// Fonction de hachage du mot de passe
+// Nombre de tours de hachage
 const SALT_ROUNDS = 10;
 
+// Fonction pour hacher le mot de passe
 export async function hashPassword(password) {
     try {
+        // Utilisation de bcrypt pour hacher le mot de passe avec le nombre de tours défini
         const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
         return hashedPassword;
     } catch (error) {
         throw new Error('Erreur lors du hachage du mot de passe');
     }
-}
+};
