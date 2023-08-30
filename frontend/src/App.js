8353d2fabbbbf73273c1583950c28d2c5afa5017
Romane Valenti <romane.valenti@gmail.com>
2023-08-30 16:20:36 +0200

    Front React

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginSignUp from './components/loginSignup/LoginSignup.jsx';
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
