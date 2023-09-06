8e359e938898b6e7f23fddaea1e3a510cb0a9e96
Romane Valenti <romane.valenti@gmail.com>
2023-09-06 17:50:03 +0200

    MainHomePage.jsx/MainHomePage.css
    Button du main dans le HomePage

--- frontend/src/components/homePage/MainHomePage.jsx
+++ frontend/src/components/homePage/MainHomePage.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import finance_tracker from '../../assets/finance_tracker.jpeg';
 import '../../style/homePage/MainHomePage.css';
+import { TbPigMoney } from 'react-icons/tb';
+import {BsCalendar4Week} from 'react-icons/bs';
+import {RiShoppingCartLine} from 'react-icons/ri';
+
+
 
 export default function MainHomePage() {
   return (
@@ -21,12 +26,20 @@ export default function MainHomePage() {
             </p>
             <div className="container-button">
                 <div className="small-button-blue">
-                    <div className="button">
+                    <div className="button-blue">
                         <p className="get">Se connecter</p>
                     </div>
                 </div>
-
-
+                <div className="small-button-yellow">
+                    <div className="button-yellow">
+                        <p className="create-account">Créer un compte</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container-logoBox">
+                <TbPigMoney/>
+                <BsCalendar4Week/>
+                <RiShoppingCartLine/>
             </div>
             
       </div>
