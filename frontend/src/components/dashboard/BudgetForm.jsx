2072d19153319cb83f963a1bb9a2954a25229fcd
Romane Valenti <romane.valenti@gmail.com>
2023-09-17 16:36:12 +0200

    Mise à jour automatique du budget
    BudgetForm.jsx/MainDasboardTitle.jsx

--- frontend/src/components/dashboard/BudgetForm.jsx
+++ frontend/src/components/dashboard/BudgetForm.jsx
@@ -1,71 +1,68 @@
-
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import axios from 'axios';
 
+export default function BudgetForm(props) {
+  const { user, setUser } = useAuth();
+  const [budgetAmount, setBudgetAmount] = useState('');
+  const [budgetPeriodStart, setBudgetPeriodStart] = useState('');
+  const [budgetPeriodEnd, setBudgetPeriodEnd] = useState('');
+  const [categoryId, setCategoryId] = useState('');
+  const [categories, setCategories] = useState([]);
 
-export default function BudgetForm() {
-    const { user, setUser } = useAuth();
-
-    const [budgetAmount, setBudgetAmount] = useState('');
-    const [budgetPeriodStart, setBudgetPeriodStart] = useState('');
-    const [budgetPeriodEnd, setBudgetPeriodEnd] = useState('');
-    const [categoryId, setCategoryId] = useState('');
-    const [categories, setCategories] = useState([]);
-
-    useEffect(() => {
-        const fetchCategories = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/budget/getCategories');
-                console.log('Response from fetchCategories:', response);
-                if (response.status === 200) {
-                    const data = response.data;
-                    console.log('Categories data:', data);
-                    setCategories(data);
-                } else {
-                    console.error('Erreur lors du chargement des catégories');
-                }
-                
-            } catch (error) {
-                console.error('Erreur lors du chargement des catégories :', error);
-            }
-        };
-
-        fetchCategories();
-    }, []);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const budgetData = {
-            budget_amount: parseFloat(budgetAmount),
-            budget_period_start: budgetPeriodStart,
-            budget_period_end: budgetPeriodEnd,
-            category_id: parseInt(categoryId),
-            user_id: user ? user.id : '', // Utilisation de l'ID de l'utilisateur connecté s'il est disponible
-        };
-
-        try {
-            
-            // Envoyez les données du formulaire au serveur pour créer le budget en utilisant Axios
-            const response = await axios.post('http://localhost:8000/budget/createOneBudget', budgetData);
-            console.log('Response from createOneBudget:', response); // Log de la réponse du serveur
-
-            if (response.status === 200) {
-                // Budget créé avec succès
-                console.log('Budget créé avec succès');
-                // Réinitialisez les champs du formulaire si nécessaire
-                setBudgetAmount('');
-                setBudgetPeriodStart('');
-                setBudgetPeriodEnd('');
-                setCategoryId('');
-            } else {
-                console.error('Erreur lors de la création du budget');
-            }
-        } catch (error) {
-            console.error('Erreur lors de la création du budget :', error);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/budget/getCategories');
+        console.log('Response from fetchCategories:', response);
+        if (response.status === 200) {
+          const data = response.data;
+          console.log('Categories data:', data);
+          setCategories(data);
+        } else {
+          console.error('Erreur lors du chargement des catégories');
         }
+        
+      } catch (error) {
+        console.error('Erreur lors du chargement des catégories :', error);
+      }
     };
+
+    fetchCategories();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const budgetData = {
+      budget_amount: parseFloat(budgetAmount),
+      budget_period_start: budgetPeriodStart,
+      budget_period_end: budgetPeriodEnd,
+      category_id: parseInt(categoryId),
+      user_id: user ? user.id : '',
+    };
+
+    try {
+      const response = await axios.post('http://localhost:8000/budget/createOneBudget', budgetData);
+
+      if (response.status === 200) {
+        console.log('Budget créé avec succès');
+
+        // Mettez à jour le montant total du budget dans le composant parent
+        props.updateTotalMonthlyBudget(parseFloat(budgetAmount));
+
+        // Réinitialisez les champs du formulaire si nécessaire
+        setBudgetAmount('');
+        setBudgetPeriodStart('');
+        setBudgetPeriodEnd('');
+        setCategoryId('');
+      } else {
+        console.error('Erreur lors de la création du budget');
+      }
+    } catch (error) {
+      console.error('Erreur lors de la création du budget :', error);
+    }
+  };
 
   return (
     <div className="budgetForm">
--- frontend/src/components/dashboard/MainDasboardTitle.jsx
+++ frontend/src/components/dashboard/MainDasboardTitle.jsx
@@ -2,58 +2,51 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth } from '../../context/AuthContext';
 import '../../style/dashboard/HeaderDashboard.css';
+import BudgetForm from './BudgetForm'; // Importez BudgetForm
 
 export default function MainDasboardTitle() {
   const { user } = useAuth();
   const [totalMonthlyBudget, setTotalMonthlyBudget] = useState(null);
   const [totalExpenseAmount, setTotalExpenseAmount] = useState(null);
-  const [remainingBudget, setRemainingBudget] = useState(null); // État pour le budget restant
+  const [remainingBudget, setRemainingBudget] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
-
-
 
   useEffect(() => {
     if (user) {
-      // Utilisez user.userId pour extraire l'ID de l'utilisateur
       const userId = user.id;
 
-      // Effectuez une requête GET pour récupérer le total du budget mensuel de l'utilisateur
       axios
         .get(`http://localhost:8000/budget/totalMonthlyBudget/${userId}`)
         .then((response) => {
-          // Mettez à jour l'état local avec le total du budget mensuel récupéré
           setTotalMonthlyBudget(response.data.totalMonthlyBudget);
         })
         .catch((error) => {
           console.error('Erreur lors de la récupération du total du budget mensuel:', error);
         });
 
-      // Effectuez une requête GET pour récupérer le total des dépenses réelles de l'utilisateur
       axios
         .get(`http://localhost:8000/transaction/totalExpenseAmount/${userId}`)
         .then((response) => {
-
-          // Mettez à jour l'état local avec le total des dépenses réelles récupéré
           setTotalExpenseAmount(response.data);
-          
         })
         .catch((error) => {
           console.error('Erreur lors de la récupération du total des dépenses réelles:', error);
         });
-
     }
   }, [user]);
 
   useEffect(() => {
     if (totalMonthlyBudget !== null && totalExpenseAmount !== null) {
-      // Calculez le budget restant en soustrayant les dépenses aux revenus mensuels
       const remaining = totalMonthlyBudget - totalExpenseAmount;
       setRemainingBudget(remaining);
     }
   }, [totalMonthlyBudget, totalExpenseAmount]);
 
+  const updateTotalMonthlyBudget = (newBudgetAmount) => {
+    // Mettez à jour le montant total du budget ici
+    setTotalMonthlyBudget(newBudgetAmount);
+  };
 
-  // Fonction pour mettre en majuscule la première lettre d'une chaîne
   const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
@@ -97,10 +90,11 @@ export default function MainDasboardTitle() {
             </div>
             <div className='remainingBudget'>
             {remainingBudget !== null ? remainingBudget : 'Calcul en cours...'}
-
             </div>
           </div>
         </div>
+        {/* Intégration du formulaire de création de budget */}
+        <BudgetForm updateTotalMonthlyBudget={updateTotalMonthlyBudget} />
       </div>
     </main>
   );
