6a1f91a7876ede834a0c7805a24ebe2da38cba95
Romane Valenti <romane.valenti@gmail.com>
2023-09-23 19:44:57 +0200

    Footer dans la page transaction
    Transactions.jsx

--- frontend/src/components/transactions/Transactions.jsx
+++ frontend/src/components/transactions/Transactions.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderDashBoard from '../dashboard/HeaderDashBoard'
 import MainTransactions from './MainTransactions'
+import Footer from '../dashboard/Footer';
 
 
 export default function Transactions() {
@@ -9,6 +10,7 @@ export default function Transactions() {
     <div>
         <HeaderDashBoard/>
         <MainTransactions/>
+        <Footer/>
     </div>
   )
 }
