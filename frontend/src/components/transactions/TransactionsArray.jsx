d55ff7dc693a5019cac95f47ca57f9cd4bb873c3
Romane Valenti <romane.valenti@gmail.com>
2023-09-27 17:28:35 +0200

    Array Recette
    TransactionsArray.jsx
    TransactionsArray.css

--- frontend/src/components/transactions/TransactionsArray.jsx
+++ frontend/src/components/transactions/TransactionsArray.jsx
@@ -13,7 +13,8 @@ function formatDate(dateString) {
 
 export default function TransactionArray() {
   const { user } = useAuth();
-  const [transactions, setTransactions] = useState([]);
+  const [expenseTransactions, setExpenseTransactions] = useState([]);
+  const [incomeTransactions, setIncomeTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentMonthIndex, setCurrentMonthIndex] = useState(new Date().getMonth());
@@ -31,7 +32,7 @@ export default function TransactionArray() {
   };
 
   const updateTransactionInArray = (updatedTransaction) => {
-    setTransactions((prevTransactions) => {
+    setExpenseTransactions((prevTransactions) => {
       const updatedIndex = prevTransactions.findIndex(
         (transaction) => transaction.id_transaction === updatedTransaction.id_transaction
       );
@@ -47,7 +48,7 @@ export default function TransactionArray() {
   };
 
   const deleteTransactionFromArray = (transactionId) => {
-    setTransactions((prevTransactions) => {
+    setExpenseTransactions((prevTransactions) => {
       const updatedTransactions = prevTransactions.filter(
         (transaction) => transaction.id_transaction !== transactionId
       );
@@ -55,21 +56,46 @@ export default function TransactionArray() {
     });
   };
 
-  const loadTransactions = (year, month) => {
+  const loadExpenseTransactions = (year, month) => {
     if (user) {
       const userId = user.id;
       axios
         .get(`http://localhost:8000/transaction/history/${userId}?year=${year}&month=${month}&type=dépense`)
         .then((response) => {
           const filteredTransactions = response.data.filter((transaction) => {
-            const transactionDate = new Date(transaction.date);
-            return transactionDate.getFullYear() === year && transactionDate.getMonth() === month - 1;
+            // Filtrer uniquement les transactions de type "dépense"
+            return transaction.type_transaction === 'dépense' && 
+                   new Date(transaction.date).getFullYear() === year && 
+                   new Date(transaction.date).getMonth() === month - 1;
           });
-          setTransactions(filteredTransactions);
+          setExpenseTransactions(filteredTransactions);
           setLoading(false);
         })
         .catch((error) => {
-          console.error('Erreur lors de la récupération des transactions :', error);
+          console.error('Erreur lors de la récupération des transactions de dépenses :', error);
+          setError(error);
+          setLoading(false);
+        });
+    }
+  };
+
+  const loadIncomeTransactions = (year, month) => {
+    if (user) {
+      const userId = user.id;
+      axios
+        .get(`http://localhost:8000/transaction/history/${userId}?year=${year}&month=${month}&type=recette`)
+        .then((response) => {
+          const filteredTransactions = response.data.filter((transaction) => {
+            // Filtrer uniquement les transactions de type "recette"
+            return transaction.type_transaction === 'recette' && 
+                   new Date(transaction.date).getFullYear() === year && 
+                   new Date(transaction.date).getMonth() === month - 1;
+          });
+          setIncomeTransactions(filteredTransactions);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la récupération des transactions de recettes :', error);
           setError(error);
           setLoading(false);
         });
@@ -80,7 +106,8 @@ export default function TransactionArray() {
     const currentDate = new Date();
     const year = currentDate.getFullYear();
     const month = currentMonthIndex + 1;
-    loadTransactions(year, month);
+    loadExpenseTransactions(year, month);
+    loadIncomeTransactions(year, month);
   }, [user, currentMonthIndex]);
 
   if (loading) {
@@ -93,35 +120,66 @@ export default function TransactionArray() {
 
   return (
     <main className="mainArrayTransaction">
-      <h2>Dépenses</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Catégorie</th>
-            <th>+/-</th>
-            <th>Modifier</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions
-            .filter((transaction) => transaction.type_transaction === 'dépense')
-            .map((filteredTransaction) => (
-              <tr key={filteredTransaction.id_transaction}>
-                <td>{formatDate(filteredTransaction.date)}</td>
-                <td>{filteredTransaction.description}</td>
-                <td>{filteredTransaction.category.categoryName}</td>
-                <td className="text-red">-{filteredTransaction.transaction_amount}</td>
+      <div className="expenseTableContainer">
+        <h2>Dépenses</h2>
+        <table className='expenseTable'>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Description</th>
+              <th>Catégorie</th>
+              <th>+/-</th>
+              <th>Modifier</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenseTransactions.map((transaction) => (
+              <tr key={transaction.id_transaction}>
+                <td>{formatDate(transaction.date)}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.category.categoryName}</td>
+                <td className="text-red">-{transaction.transaction_amount}</td>
                 <td>
-                  <button onClick={() => handleOpenUpdateForm(filteredTransaction)} className="icon-buttonUpdate">
+                  <button onClick={() => handleOpenUpdateForm(transaction)} className="icon-buttonUpdate">
                     <AiOutlineEdit />
                   </button>
                 </td>
               </tr>
             ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="incomeTableContainer">
+        <h2>Recettes</h2>
+        <table className='incomeTable'>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Description</th>
+              <th>Catégorie</th>
+              <th>+/-</th>
+              <th>Modifier</th>
+            </tr>
+          </thead>
+          <tbody>
+            {incomeTransactions.map((transaction) => (
+              <tr key={transaction.id_transaction}>
+                <td>{formatDate(transaction.date)}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.category.categoryName}</td>
+                <td className="text-green">+{transaction.transaction_amount}</td>
+                <td>
+                  <button onClick={() => handleOpenUpdateForm(transaction)} className="icon-buttonUpdate">
+                    <AiOutlineEdit />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      
 
       {isUpdateModalOpen && selectedTransaction && (
         <div className="update-modal">
@@ -129,8 +187,8 @@ export default function TransactionArray() {
             transactionId={selectedTransaction.id_transaction}
             onClose={handleCloseUpdateForm}
             onUpdateSuccess={updateTransactionInArray}
-            onDeleteSuccess={deleteTransactionFromArray} // Ajoutez cette ligne
-            loadTransactions={loadTransactions}
+            onDeleteSuccess={deleteTransactionFromArray}
+            loadTransactions={loadExpenseTransactions}
             currentMonthIndex={currentMonthIndex}
           />
         </div>
