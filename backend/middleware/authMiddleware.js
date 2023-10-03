c2628d6933ac40a3893b4fe1b17912398143f92a
Romane Valenti <romane.valenti@gmail.com>
2023-10-03 21:15:43 +0200

    Amélioration middleware
    authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,49 +1,52 @@
-import jwt from 'jsonwebtoken';
-import jwtSecret from '../config.js'; // Importez jwtSecret depuis le fichier de configuration
 import authService from '../services/authService.js';
 
+// Middleware pour valider les entrées (email et mot de passe) lors de l'authentification
 export const validateInputs = (req, res, next) => {
     const { email, password } = req.body;
-    
+
+    // Vérification si l'email et le mot de passe sont présents dans la requête
     if (!email || !password) {
-        return res.status(401).json({ message: "Les champs email et password sont requis." });
+        return res.status(400).json({ message: "Les champs email et password sont requis." });
     }
-    
+
+    // Si les champs sont valides, middleware suivant
     next();
 };
 
+// Middleware pour l'authentification des utilisateurs
 export const authenticate = async (req, res, next) => {
     try {
         let user;
 
         // Vérifier si le token JWT est présent dans l'en-tête d'authorization
         const token = req.headers.authorization;
+
         if (token) {
-            try{
-                console.log("Token JWT reçu:", token); 
+            try {
+                // Vérification du token JWT en utilisant le service d'authentification
                 user = await authService.verifyJWT(token.substring("Bearer ".length));
+
+                // Si l'utilisateur associé au token n'existe pas, renvoyez une erreur
                 if (!user) {
                     return res.status(401).json({ message: "L'utilisateur associé au token n'existe pas." });
-                }else{
-                    console.log("Utilisateur vérifier avec succès", user);
-                   
                 }
-            }catch (error) {
+            } catch (error) {
                 console.error("Erreur de vérification du token:", error);
                 return res.status(401).json({ message: "Token invalide" });
             }
         } else {
-            // Sinon, utilisez l'authentification basée sur l'email et le mot de passe
+            // Si aucun token n'est présent, effectuez l'authentification basée sur l'email et le mot de passe
             user = await authService.authenticateUser(req.body.email, req.body.password);
+
+            if (!user) {
+                return res.status(401).json({ message: "L'authentification a échoué : informations d'identification incorrectes." });
+            }
         }
 
-        if (!user) {
-            return res.status(401).json({ message: "L'authentification a échoué : informations d'identification incorrectes." });
-        }
-        
+        // Stockez les informations de l'utilisateur dans req.user pour une utilisation ultérieure
         req.user = user;
-        console.log("Utilisateur yoyo",req.user)
-        console.log("Utilisateur ajouté à req avec succès", user); // Ajout du console.log ici
+
+        // middleware suivant
         next();
     } catch (error) {
         console.error("Authentication failed:", error);
