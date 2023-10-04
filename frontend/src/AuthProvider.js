db728fb4d593c5dba3236c7e5d61cc34775c42e5
Romane Valenti <romane.valenti@gmail.com>
2023-10-04 17:53:32 +0200

    Amélioration provider
    AuthProvider.js
    index.js

--- frontend/src/AuthProvider.js
+++ frontend/src/AuthProvider.js
@@ -1,12 +1,16 @@
-// AuthProvider.js
 import React, { createContext, useContext, useState } from 'react';
 
+// Création d'un contexte d'authentification
 const AuthContext = createContext();
 
+// Définition du composant AuthProvider qui enveloppe d'autres composants
 export const AuthProvider = ({ children }) => {
+  // État local pour stocker les informations de l'utilisateur
   const [user, setUser] = useState(null);
+  // État local pour gérer la visibilité du formulaire de connexion
   const [isLoginFormVisible, setIsLoginFormVisible] = useState(false);
 
+  // Fonction pour basculer la visibilité du formulaire de connexion
   const toggleLoginForm = () => {
     setIsLoginFormVisible(!isLoginFormVisible);
   };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,13 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './AuthProvider'; // Importez le AuthProvider
+import { AuthProvider } from './AuthProvider'; 
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Enveloppez votre application avec AuthProvider */}
     <AuthProvider>
       <BrowserRouter>
         <App />
